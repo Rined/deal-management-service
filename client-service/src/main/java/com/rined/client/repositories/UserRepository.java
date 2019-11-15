132bd79cef08aad51c335ff350c3729d2cec907e
Anton Petrov <antoxa-ac@list.ru>
2019-11-15 23:36:21 +0300

    remove redundant service

--- client-service/src/main/java/com/rined/client/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rined.client.repositories;
-
-import com.rined.client.model.collections.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String>, UserRepositoryCustom {
-
-}
